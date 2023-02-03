e93d94d8b152865901f4f0688604b76335985ac3
Felipe Oliver <felioliver96@gmail.com>
2023-02-03 14:57:35 +0100

    merge main

--- src/components/unverified.tsx
+++ src/components/unverified.tsx
@@ -1,14 +1,10 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import {CardActionArea, Grid, Button, CardActions} from '@mui/material';
 import '../App.css';
 import {useTranslation} from "react-i18next";
 import './verify.css';
 
 export default function Unverified() {
+    const {t} = useTranslation()
     return (
         
         <div className="container-fluid py-5">
--- src/components/verified.tsx
+++ src/components/verified.tsx
@@ -1,9 +1,4 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import {CardActionArea, Grid, Button, CardActions} from '@mui/material';
 import '../App.css';
 import {useTranslation} from "react-i18next";
 import './verify.css';
