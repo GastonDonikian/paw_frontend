b7c00ba1bba2eb6538e5c9e31d4078ec0a762f13
Lu <ldigon@itba.edu.ar>
2023-07-15 18:45:21 -0300

    error pages

---
+++ src/Pages/Errors/Error403.tsx
@@ -0,0 +1,42 @@
+import {intl} from "../../i18n/i18n";
+import './error.css';
+
+export default function Error403(){
+
+
+    return(
+
+<div className="error-content">
+    <div className="container">
+        <div className="row">
+            <div className="col-md-12 ">
+                <div className="error-text">
+                    <h1 className="error">{intl.formatMessage({ id: 'error_403'})}</h1>
+                    <div className="im-sheep">
+                        <div className="top">
+                            <div className="body"></div>
+                            <div className="head">
+                                <div className="im-eye one"></div>
+                                <div className="im-eye two"></div>
+                                <div className="im-ear one"></div>
+                                <div className="im-ear two"></div>
+                            </div>
+                        </div>
+                        <div className="im-legs">
+                            <div className="im-leg"></div>
+                            <div className="im-leg"></div>
+                            <div className="im-leg"></div>
+                            <div className="im-leg"></div>
+                        </div>
+                    </div>
+                    <h4>{intl.formatMessage({ id: 'error_problem'})}</h4>
+                    <p>{intl.formatMessage({ id: 'error_privilege'})}</p>
+                    <a href="/" className="btn btn-primary btn-round">{intl.formatMessage({ id: 'error_back'})}</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+        
+    );
+};
